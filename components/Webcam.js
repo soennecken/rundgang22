57a0c4eb951583db60b95a5c1c39e5599185ee6a
Sonja Schwarz <sonjaschwarz@sonjas-mbp.speedport_w_724v_typ_a_05011603_06_003>
2022-07-03 12:48:41 +0200

    ...

--- components/Webcam.js
+++ components/Webcam.js
@@ -10,7 +10,7 @@ const Webcam = () => {
           src="https://player.vimeo.com/video/76979871?h=443ac71bde&background=true"
           width="440"
           height="1138"
-          frameborder="0"
+          frameBorder="0"
           allow="autoplay; fullscreen; picture-in-picture"
           allowfullscreen
         ></iframe>
