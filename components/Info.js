fb29b9e93fa2bb630b85b19e81463ec10276aa43
Sonja Schwarz <sonjaschwarz@sonjas-mbp.speedport_w_724v_typ_a_05011603_06_003>
2022-07-03 13:46:29 +0200

    ...

--- components/Info.js
+++ components/Info.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Marquee from "react-fast-marquee";
 
 import logo from "../public/images/RG22_Logo.png";
 
@@ -17,13 +18,49 @@ const Info = () => {
     <main>
       <div className="logo">
         <img src={logo} />
-      </div>   
-    <div
-      style={open ? active : inactive}
-      className="info"
-      onClick={() => setOpen(!open)}>
-      <div className={open ? "infoContent" : "hide"}>Test</div>
-    </div>
+      </div>
+      <div
+        style={open ? active : inactive}
+        className="info"
+        onClick={() => setOpen(!open)}
+      >
+        <Marquee gradient={false}>
+          <div className="liveTicker">
+            +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK
+            LIVE +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK
+            LIVE+++ABK LIVE +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK
+            LIVE+++ABK LIVE+++ABK LIVE +++ABK LIVE++++++ABK LIVE+++ABK
+            LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE +++ABK LIVE++++++ABK
+            LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK
+            LIVE +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK
+            LIVE+++ABK LIVE +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK
+            LIVE+++ABK LIVE+++ABK LIVE +++ABK LIVE++++++ABK LIVE+++ABK
+            LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE +++ABK LIVE++++++ABK
+            LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE            +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK
+            LIVE +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK
+            LIVE+++ABK LIVE +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK
+            LIVE+++ABK LIVE+++ABK LIVE +++ABK LIVE++++++ABK LIVE+++ABK
+            LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE +++ABK LIVE++++++ABK
+            LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE            +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK
+            LIVE +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK
+            LIVE+++ABK LIVE +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK
+            LIVE+++ABK LIVE+++ABK LIVE +++ABK LIVE++++++ABK LIVE+++ABK
+            LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE +++ABK LIVE++++++ABK
+            LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE            +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK
+            LIVE +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK
+            LIVE+++ABK LIVE +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK
+            LIVE+++ABK LIVE+++ABK LIVE +++ABK LIVE++++++ABK LIVE+++ABK
+            LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE +++ABK LIVE++++++ABK
+            LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE            +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK
+            LIVE +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK
+            LIVE+++ABK LIVE +++ABK LIVE++++++ABK LIVE+++ABK LIVE+++ABK
+            LIVE+++ABK LIVE+++ABK LIVE +++ABK LIVE++++++ABK LIVE+++ABK
+            LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE +++ABK LIVE++++++ABK
+            LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE+++ABK LIVE
+          </div>
+        </Marquee>
+        <div className={open ? "infoContent" : "hide"}>Test</div>
+      </div>
     </main>
   );
 };
