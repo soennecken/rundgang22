a9cd982b514724e3fecba83c55e2352c7d703a9e
Sonja Schwarz <sonjaschwarz@sonjas-mbp.speedport_w_724v_typ_a_05011603_06_003>
2022-07-04 18:04:00 +0200

    ...

--- components/Daten.js
+++ components/Daten.js
@@ -1,41 +1,57 @@
-import React, { useEffect, useState, useRef  } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 import Graph from "./Graph";
-import Horoskop from "./Horoskop"
+import Horoskop from "./Horoskop";
 
 const Daten = () => {
-  const [weatherData, setWeatherData] = useState([{}])
+  const [height, setHeight] = useState("");
 
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    setHeight(`${elementRef.current.clientHeight}`);
+
+  }, []);
+
+  console.log(height);
 
   return (
     <main className="daten">
-
-      <div className="horoskopWrapper">
-        <Horoskop zodiac={"cancer"}/>
-        <Horoskop zodiac={"leo"}/>
-        <Horoskop zodiac={"virgo"}/>
-        <Horoskop zodiac={"libra"}/>
-        <Horoskop zodiac={"scorpio"}/>
-        <Horoskop zodiac={"sagittarius"}/>
-        <Horoskop zodiac={"capricorn"}/>
-        <Horoskop zodiac={"aquarius"}/>
-        <Horoskop zodiac={"pisces"}/>
-        <Horoskop zodiac={"aries"}/>
-        <Horoskop zodiac={"taurus"}/>
-        <Horoskop zodiac={"gemini"}/>
+      <div className="horoskopWrapper"  >
+        <div ref={elementRef}>
+          <Horoskop zodiac={"cancer"} height={height}/>
+          <Horoskop zodiac={"leo"} height={height}/>
+          <Horoskop zodiac={"virgo"} height={height}/>
+          <Horoskop zodiac={"libra"} height={height}/>
+          <Horoskop zodiac={"scorpio"} height={height}/>
+          <Horoskop zodiac={"sagittarius"} height={height}/>
+          <Horoskop zodiac={"capricorn"} height={height}/>
+          <Horoskop zodiac={"aquarius"} height={height}/>
+          <Horoskop zodiac={"pisces"} height={height}/>
+          <Horoskop zodiac={"aries"} height={height}/>
+          <Horoskop zodiac={"taurus"} height={height}/>
+          <Horoskop zodiac={"gemini"} height={height}/>
+        </div>
       </div>
 
-      <div className="stocksWrapper" >
+      <div className="stocksWrapper">
+        <div className="graphWrapper">
+          <Graph symbol={"GOOG"} stockName={"Alphabet Inc"} />
+          <Graph symbol={"WMT"} stockName={"Walmart Inc"} />
+        </div>
         <div className="graphWrapper">
           <Graph symbol={"DAI.DEX"} stockName={"Daimler AG"} />
           <Graph symbol={"AAPL"} stockName={"Apple Corp."} />
         </div>
-        <div className="graphWrapper" >
-          <Graph symbol={"DAI.DEX"} stockName={"Daimler AG"} />
-          <Graph symbol={"AAPL"} stockName={"Apple Corp."} />
+        <div className="graphWrapper">
+          <Graph symbol={"PEP"} stockName={"PepsiCo, Inc"} />
+          <Graph symbol={"BABA"} stockName={"Alibaba Group Holding"} />
+        </div>
+        <div className="graphWrapper">
+          <Graph symbol={"ADBE"} stockName={"Adobe Inc."} />
+          <Graph symbol={"MCD"} stockName={"McDonald's Corporation"} />
         </div>
       </div>
-      
     </main>
   );
 };
--- components/Horoskop.js
+++ components/Horoskop.js
@@ -1,24 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Horoskop = (props) => {
   const [zodiac, setZodiac] = useState({});
-  const [sign, setSign] = useState("")
+  const [sign, setSign] = useState("");
+  const [running, setRunning] = useState(false);
+  const [style, setStyle] = useState("");
 
-  const updateSign = () => { 
-
-    props.zodiac === "cancer" ? setSign("") : ""
-    props.zodiac === "leo" ? setSign("") : ""
-    props.zodiac === "virgo" ? setSign("") : ""
-    props.zodiac === "libra" ? setSign("") : ""
-    props.zodiac === "scorpio" ? setSign("") : ""
-    props.zodiac === "sagittarius" ? setSign("") : ""
-    props.zodiac === "capricorn" ? setSign("") : ""
-    props.zodiac === "aquarius" ? setSign("") : ""
-    props.zodiac === "pisces" ? setSign("") : ""
-    props.zodiac === "aries" ? setSign("") : ""
-    props.zodiac === "taurus" ? setSign("") : ""
-    props.zodiac === "gemini" ? setSign("") : ""
-  }
+  const updateSign = () => {
+    props.zodiac === "cancer" ? setSign("") : "";
+    props.zodiac === "leo" ? setSign("") : "";
+    props.zodiac === "virgo" ? setSign("") : "";
+    props.zodiac === "libra" ? setSign("") : "";
+    props.zodiac === "scorpio" ? setSign("") : "";
+    props.zodiac === "sagittarius" ? setSign("") : "";
+    props.zodiac === "capricorn" ? setSign("") : "";
+    props.zodiac === "aquarius" ? setSign("") : "";
+    props.zodiac === "pisces" ? setSign("") : "";
+    props.zodiac === "aries" ? setSign("") : "";
+    props.zodiac === "taurus" ? setSign("") : "";
+    props.zodiac === "gemini" ? setSign("") : "";
+  };
 
   const options = {
     method: "POST",
@@ -28,8 +29,8 @@ const Horoskop = (props) => {
     },
   };
 
-  useEffect(() => {
-    updateSign()
+  useEffect(  () => {
+    updateSign();
 
     fetch(
       `https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=${props.zodiac}&day=today`,
@@ -39,46 +40,74 @@ const Horoskop = (props) => {
       .then((response) => setZodiac(response))
       .catch((err) => console.error(err));
 
+      setRunning(true)
+
   }, []);
 
-  console.log(zodiac);
+  const style01 = {
+    transform: `translateY(-${props.height}px)`,
+  };
+
+  const none = {};
+
+  console.log(props.height)
 
   return (
-    <div className="horoskopOuter">
-      <div className="zodiacWrapper">
+    <div className="horoskopOuter" style={running ? style01 : none}>
+      <div className="zodiacWrapper" >
         <div>{zodiac.current_date}</div>
         <div className="zodiacHeader">
           <div className="zodiacSign">{sign}</div>
-          <h1>{props.zodiac}</h1>{zodiac.date_range}
+          <h1>{props.zodiac}</h1>
+          {zodiac.date_range}
         </div>
         <p>{zodiac.description}</p>
         <table>
-            <tbody>
-                <tr>
-                    <td><p>Color</p></td>
-                    <td><p>{zodiac.color}</p></td>
-                </tr>
-                <tr>
-                    <td><p>Compatibility</p></td>
-                    <td><p>{zodiac.compatibility}</p></td>
-                </tr>
-                <tr>
-                    <td><p>Lucky Number</p></td>
-                    <td><p>{zodiac.lucky_number}</p></td>
-                </tr>
-                <tr>
-                    <td><p>Lucky Time</p></td>
-                    <td><p>{zodiac.lucky_time}</p></td>
-                </tr>
-                <tr>
-                    <td><p>Mood</p></td>
-                    <td><p>{zodiac.mood}</p></td>
-                </tr>
-            </tbody>
+          <tbody>
+            <tr>
+              <td>
+                <p>Color</p>
+              </td>
+              <td>
+                <p>{zodiac.color}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>Compatibility</p>
+              </td>
+              <td>
+                <p>{zodiac.compatibility}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>Lucky Number</p>
+              </td>
+              <td>
+                <p>{zodiac.lucky_number}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>Lucky Time</p>
+              </td>
+              <td>
+                <p>{zodiac.lucky_time}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>Mood</p>
+              </td>
+              <td>
+                <p>{zodiac.mood}</p>
+              </td>
+            </tr>
+          </tbody>
         </table>
-
       </div>
-      <hr/>
+      <hr />
     </div>
   );
 };
